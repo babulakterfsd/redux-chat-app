068c37205eaf79e5a14312fe6e1f10b0f547bb65
Babul Akter <babulakterfsd@gmail.com>
2022-12-21 14:35:10 +0600

    api features added and store configured

--- src/rtk/features/api/apiSlice.js
+++ src/rtk/features/api/apiSlice.js
@@ -4,6 +4,7 @@ export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
         baseUrl: `http://localhost:9000`,
+        // baseUrl: process.env.REACT_APP_API,
     }),
     tagTypes: [],
     endpoints: (builder) => ({
