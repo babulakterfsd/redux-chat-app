4521d9c67e2dbcf3f22dc713ba31c060f27b271b
Babul Akter <babulakterfsd@gmail.com>
2022-12-27 20:32:33 +0600

    login feature implemented

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,11 +1,54 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import logoImage from '../assets/images/lws-logo-light.svg';
 import Error from '../components/ui/Error';
+import { useLoginMutation } from '../rtk/features/auth/authAPI';
 
 export default function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const [login, { data, isLoading, isSuccess, error: responseError }] = useLoginMutation();
+
+    useEffect(() => {
+        if (responseError?.data) {
+            setError(responseError?.data);
+        }
+        if (isSuccess) {
+            setTimeout(() => {
+                navigate('/inbox');
+            }, 1000);
+        }
+    }, [data, responseError, navigate, isSuccess]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setError('');
+        login({
+            email,
+            password,
+        });
+    };
+
+    if (isSuccess) {
+        toast.success('Login Successfull !', {
+            toastId: 'success1',
+            position: 'top-right',
+            autoClose: 1000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: 'light',
+        });
+    }
+
     return (
         <div className="grid place-items-center h-screen bg-[#F9FAFB">
             <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -22,7 +65,7 @@ export default function Login() {
                             Sign in to your account
                         </h2>
                     </div>
-                    <form className="mt-8 space-y-6" action="#" method="POST">
+                    <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
                         <input type="hidden" name="remember" value="true" />
                         <div className="rounded-md shadow-sm -space-y-px">
                             <div>
@@ -37,6 +80,8 @@ export default function Login() {
                                     required
                                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
                                     placeholder="Email address"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
                                 />
                             </div>
                             <div>
@@ -51,6 +96,8 @@ export default function Login() {
                                     required
                                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
                                     placeholder="Password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
                                 />
                             </div>
                         </div>
@@ -70,6 +117,7 @@ export default function Login() {
                             <button
                                 type="submit"
                                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
+                                disabled={isLoading}
                             >
                                 Sign in
                             </button>
