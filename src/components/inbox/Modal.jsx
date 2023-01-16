656a2bdf2e391007e7b7c1c159ab82e8e7beec8f
Babul Akter <babulakterfsd@gmail.com>
2023-01-16 15:49:07 +0600

    debounce fn added on modal while sending message

--- src/components/inbox/Modal.jsx
+++ src/components/inbox/Modal.jsx
@@ -1,6 +1,31 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
+
+import { useState } from 'react';
+import isVlaidEmail from '../../utils/isValidEmail';
+
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 export default function Modal({ open, control }) {
+    const [to, setTo] = useState('');
+    const [message, setMessage] = useState('');
+
+    const debounceHandler = (fn, delay) => {
+        let timeOutId;
+        return (...args) => {
+            clearTimeout(timeOutId);
+            timeOutId = setTimeout(() => {
+                fn(...args);
+            }, delay);
+        };
+    };
+
+    const doSearch = (value) => {
+        if (isVlaidEmail(value)) {
+            setTo(value);
+        }
+    };
+
+    const handleSearch = debounceHandler(doSearch, 500);
+
     return (
         open && (
             <>
@@ -12,8 +37,7 @@ export default function Modal({ open, control }) {
                     <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
                         Send message
                     </h2>
-                    <form className="mt-8 space-y-6" action="#" method="POST">
-                        <input type="hidden" name="remember" value="true" />
+                    <form className="mt-8 space-y-6">
                         <div className="rounded-md shadow-sm -space-y-px">
                             <div>
                                 <label htmlFor="to" className="sr-only">
@@ -22,10 +46,11 @@ export default function Modal({ open, control }) {
                                 <input
                                     id="to"
                                     name="to"
-                                    type="to"
+                                    type="email"
                                     required
                                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
                                     placeholder="Send to"
+                                    onChange={(e) => handleSearch(e.target.value)}
                                 />
                             </div>
                             <div>
@@ -35,10 +60,12 @@ export default function Modal({ open, control }) {
                                 <textarea
                                     id="message"
                                     name="message"
-                                    type="message"
+                                    type="text"
                                     required
                                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
                                     placeholder="Message"
+                                    value={message}
+                                    onChange={(e) => setMessage(e.target.value)}
                                 />
                             </div>
                         </div>
