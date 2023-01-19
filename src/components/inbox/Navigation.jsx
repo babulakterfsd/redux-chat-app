dc0f70c88144c7ced053c210ce4d815d55e64aef
Babul Akter <babulakterfsd@gmail.com>
2023-01-19 15:18:24 +0600

    loggedin users name shown in the navbar

--- src/components/inbox/Navigation.jsx
+++ src/components/inbox/Navigation.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import logoImage from '../../assets/images/lws-logo-dark.svg';
 import { userLoggedOut } from '../../rtk/features/auth/authSlice';
 
 export default function Navigation() {
     const dispatch = useDispatch();
+    const { user } = useSelector((state) => state.auth || {});
+    const { name } = user || {};
 
     const logout = () => {
         dispatch(userLoggedOut());
@@ -19,8 +21,11 @@ export default function Navigation() {
                     <Link to="/">
                         <img className="h-10" src={logoImage} alt="Learn with Sumit" />
                     </Link>
+                    <div className="flex gap-x-4 justify-center items-center">
+                        <span className="text-white font-semibold">{`You're logged in as ${name}`}</span>
+                    </div>
                     <ul>
-                        <li className="text-white">
+                        <li className="text-red-400 font-semibold p-1 rounded-md">
                             <span className="cursor-pointer" onClick={logout}>
                                 Logout
                             </span>
