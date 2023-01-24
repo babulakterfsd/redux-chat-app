e66fb08a3e0dc328137431dc74f6a0f28aab1933
Babul Akter <babulakterfsd@gmail.com>
2023-01-24 15:53:43 +0600

    security issue fixed with not found page

--- src/views/Inbox.jsx
+++ src/views/Inbox.jsx
@@ -1,8 +1,32 @@
+/* eslint-disable eqeqeq */
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import ChatBody from '../components/inbox/chatbody/ChatBody';
 import Navigation from '../components/inbox/Navigation';
 import Sidebar from '../components/inbox/Sidebar';
+import { useGetConversationsQuery } from '../rtk/features/conversations/conversationsAPI';
+import NotFound from './NotFound';
 
 export default function Inbox() {
+    const [redirect, setRedirect] = useState(false);
+    const { id } = useParams();
+    const { user } = useSelector((state) => state.auth || {});
+    const { email: loggedInUserEmail } = user || {};
+    const { data: conversations } = useGetConversationsQuery(loggedInUserEmail);
+
+    useEffect(() => {
+        const isIdMatched = conversations?.find((conv) => conv.id == id);
+        if (isIdMatched === undefined) {
+            setRedirect(true);
+        } else if (isIdMatched.id) {
+            setRedirect(false);
+        }
+    }, [id, conversations]);
+
+    if (redirect) {
+        return <NotFound />;
+    }
     return (
         <div>
             <Navigation />
--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Navigation from '../components/inbox/Navigation';
 
 function NotFound() {
     return (
-        <div className="min-h-screen flex justify-center items-center">
-            <p className="text-red-400 text-2xl font-semibold">Page Not Found ! </p>
-        </div>
+        <>
+            <Navigation />
+            <div className="min-h-screen flex justify-center items-center">
+                <p className="text-red-400 text-2xl font-semibold">Page Not Found ! </p>
+            </div>
+        </>
     );
 }
 
